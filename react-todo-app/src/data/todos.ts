147daad7f43f40c8480801dd02fc87af33ea5faa
KiBeGit <kirill.berger2002@gmail.com>
2024-08-15 17:00:11 +0200

    todo app

---
+++ react-todo-app/src/data/todos.ts
@@ -0,0 +1,11 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  { id: 1, title: "Im Schulbuch lesen", completed: false },
+  { id: 2, title: "React-Tutorial machen", completed: false },
+  {
+    id: 3,
+    title: "Mit Markus das neue Ticket zusammen anschauen",
+    completed: false,
+  },
+];
